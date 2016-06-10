5f3f88ab52e6984f8c7e7f09460d87747facf12d
Daniel Rose <daniel-rose@gmx.de>
2016-06-10 12:04:10 +0200

    For rendering a widget in cms page content the class DR\Gallery\Block\Widget\Gallery must implement the interface Magento\Widget\Block\BlockInterface.

--- Block/Widget/Gallery.php
+++ Block/Widget/Gallery.php
@@ -3,7 +3,8 @@
 namespace DR\Gallery\Block\Widget;
 
 use DR\Gallery\Block\Gallery as GalleryBlock;
+use Magento\Widget\Block\BlockInterface;
 
-class Gallery extends GalleryBlock
+class Gallery extends GalleryBlock implements BlockInterface
 {
 }
